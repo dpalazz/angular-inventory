4f43ffa9d3be4ec7d459d94d3985bd15cf2ca9a2
Dana Palazzo <dana@fretless.com>
2018-07-23 10:48:38 -0400

    Update metadata and view template for ProductDepartment component

--- src/app/product-department/product-department.component.ts
+++ src/app/product-department/product-department.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Product } from '../product.model'
 
 @Component({
-  selector: 'app-product-department',
+  selector: 'product-department',
   templateUrl: './product-department.component.html',
   styleUrls: ['./product-department.component.css']
 })
-export class ProductDepartmentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ProductDepartmentComponent {
+  @Input() product: Product;
 }
