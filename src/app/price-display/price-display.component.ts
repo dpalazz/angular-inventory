6dd98efd067bb168aa77fd2603a10c45b7d54f1f
Dana Palazzo <dana@fretless.com>
2018-07-23 10:43:39 -0400

    Add metadata to PriceDisplay component

--- src/app/price-display/price-display.component.ts
+++ src/app/price-display/price-display.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-price-display',
-  templateUrl: './price-display.component.html',
+  selector: 'price-display',
+  template: `
+    <div class="price-display">\${{ price }}</div>
+  `,
   styleUrls: ['./price-display.component.css']
 })
-export class PriceDisplayComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class PriceDisplayComponent {
+  @Input() price: number;
 }
