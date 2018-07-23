66e9c908b3e22b1831766fb973581def6dabcc9d
Dana Palazzo <dana@fretless.com>
2018-07-23 10:37:48 -0400

    Create ProductImage component

---
+++ src/app/product-image/product-image.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-image',
+  templateUrl: './product-image.component.html',
+  styleUrls: ['./product-image.component.css']
+})
+export class ProductImageComponent {
+
+  constructor() { }
+
+}
